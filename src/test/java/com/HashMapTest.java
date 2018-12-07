759e14afb3346626b03e695424db4c6fd3851159
Valentine Tvardovskiy <valentintw1999@gmail.com>
2018-12-07 04:34:07 +0200

    add some tests

--- src/test/java/com/HashMapTest.java
+++ src/test/java/com/HashMapTest.java
@@ -1,5 +1,7 @@
 package com;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,34 +10,53 @@ public class HashMapTest {
 
     private HashMap hashMap;
 
-    public void setUp() throws Exception {
+    @Before
+    public void setUp() {
         hashMap = new HashMap();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             hashMap.put(i, i);
         }
     }
 
     @Test
     public void testTrueSize() {
+        int expectedResult = 5;
+        Assert.assertEquals(expectedResult, hashMap.size());
     }
 
     @Test
     public void testGetByKey() {
+        long expectedResult = 3;
+        Assert.assertEquals(expectedResult, hashMap.get(3));
     }
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void testGetByWrongKey() {
+        int key = 55;
+        hashMap.get(key);
     }
 
     @Test
     public void testPutSameKeyButAnotherValue() {
+        long previousResult = hashMap.get(1);
+        hashMap.put(1, 10);
+        long newResult = hashMap.get(1);
+        Assert.assertNotEquals(previousResult, newResult);
     }
 
     @Test
-    public void testPutValueWithBigKey() {
+    public void testPutValueWithKeyBiggerThanCapacity() {
+        hashMap.put(122, 123);
+        long expectedResult = 123;
+        Assert.assertEquals(expectedResult, hashMap.get(122));
+
     }
 
     @Test
     public void testPutAlreadyUsedIndexButAnotherKey() {
+        long previousResult = hashMap.get(2);
+        hashMap.put(18, 555);
+        long expectedResult = hashMap.get(18);
+        Assert.assertNotEquals(previousResult, expectedResult);
     }
 }
