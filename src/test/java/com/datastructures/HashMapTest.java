5de0e3f3d3eb73ee7ee4f6504057e764c463c049
Valentine Tvardovskiy <valentintw1999@gmail.com>
2018-12-08 04:03:53 +0200

    upgrade tests

--- src/test/java/com/datastructures/HashMapTest.java
+++ src/test/java/com/datastructures/HashMapTest.java
@@ -11,31 +11,37 @@ public class HashMapTest {
     @Before
     public void setUp() {
         hashMap = new HashMap();
-        for (int i = 0; i < 50; i++) {
-            hashMap.put(i, i);
-        }
     }
 
     @Test
     public void testSize_shouldReturnProperSizeOfNonEmptyMap() {
-        int expectedResult = 50;
+        for (int i = 0; i < 30; i++) {
+            hashMap.put(i, i);
+        }
+        int expectedResult = 30;
         Assert.assertEquals(expectedResult, hashMap.size());
     }
 
     @Test
     public void testGetByKey_shouldReturnProperValue() {
-        long expectedResult = 3;
+        hashMap.put(2, 22);
+        hashMap.put(3, 33);
+        long expectedResult = 33;
         Assert.assertEquals(expectedResult, hashMap.get(3));
     }
 
     @Test(expected = NullPointerException.class)
-    public void testGetByKeyNegativeScenario_shouldNotFoundKey() {
+    public void testGetByKeyNegativeScenario_shouldReturnNull() {
+        for (int i = 0; i < 5; i++) {
+            hashMap.put(i, i);
+        }
         int key = 55;
         hashMap.get(key);
     }
 
     @Test
     public void testPutSameKeyButAnotherValue_shouldRewriteValueForThisKey() {
+        hashMap.put(1, 11);
         long previousResult = hashMap.get(1);
         hashMap.put(1, 10);
         long newResult = hashMap.get(1);
@@ -43,7 +49,7 @@ public class HashMapTest {
     }
 
     @Test
-    public void testPutValueWithKeyBiggerThanCapacity_shouldSimplyPutInFreePosition() {
+    public void testPutValueWithKeyBiggerThanInitialCapacity_shouldSimplyPutInFreePosition() {
         hashMap.put(122, 123);
         long expectedResult = 123;
         Assert.assertEquals(expectedResult, hashMap.get(122));
@@ -52,6 +58,7 @@ public class HashMapTest {
 
     @Test
     public void testPutAlreadyUsedIndexButAnotherKey_shouldSimplyPutInFreePosition() {
+        hashMap.put(2, 22);
         long previousResult = hashMap.get(2);
         hashMap.put(18, 555);
         long expectedResult = hashMap.get(18);
